cc7adf8cc73f4d9b58dcc8a2111ef4b142a11c5e
alicelanznaster <alicelanznaster@gmail.com>
2026-07-14 16:48:47 -0300

    FEAT: first commit

---
+++ src/router/index.js
@@ -0,0 +1,35 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import AvaliacaoView from '@/views/AvaliacaoView.vue'
+import RankingView from '@/views/RankingView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/avaliacao',
+      name: 'avaliacao',
+      component: AvaliacaoView,
+    },
+    {
+      path: '/ranking',
+      name: 'ranking',
+      component: RankingView,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue'),
+    },
+  ],
+})
+
+export default router
---
+++ src/views/AvaliacaoView.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <h1>Página de Avaliação - Cafés Desafio</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis quaerat veniam voluptas aperiam iusto ut nostrum amet numquam? Laudantium assumenda commodi natus exercitationem nulla repellat fugit, velit distinctio animi perferendis.</p>
+  </main>
+</template>
+
+<style scoped></style>
